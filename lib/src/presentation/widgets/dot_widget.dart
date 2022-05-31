ccd9e6b95ed9c092610c070fc807af7fa1689738
ederkach <e.derkach.pr@gmail.com>
2022-05-30 21:00:53 -0600

    refactor: End refactor home screen & common components to Widgets

--- lib/src/presentation/widgets/dot_widget.dart
+++ lib/src/presentation/widgets/dot_widget.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DotWidget extends StatelessWidget {
   const DotWidget({
     required this.dotColor,
+    required this.sizeheight,
+    required this.sizewidth,
     Key? key,
   }) : super(key: key);
 
   final Color dotColor;
+  final double sizeheight;
+  final double sizewidth;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 7.h,
-      width: 7.w,
+      height: sizeheight.h,
+      width: sizewidth.w,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: dotColor,
